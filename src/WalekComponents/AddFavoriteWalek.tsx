17a092d753dba8e4a1dc1ce27fb47d6b75048ad1
David Miko <ph4nt0mgh0sT17@gmail.com>
2022-04-05 23:52:37 +0200

    Deleted all files from the main branch.

--- src/WalekComponents/AddFavoriteWalek.tsx
+++
@@ -1,9 +0,0 @@
-const AddFavoriteWalek = () => {
-    return (
-        <>
-            <span className="mr-2">Přidat do oblíbených</span>
-        </>
-    );
-};
-
-export default AddFavoriteWalek;
--- src/search-response.ts
+++
@@ -1,7 +0,0 @@
-export interface SearchEntity {
-    Title: string;
-    Year: string;
-    imdbID: string;
-    Type: string;
-    Poster: string;
-}
