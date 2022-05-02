26f4c41d19d6c57ecbe4d3cffdd4bfc8bdb86cf2
David Miko <ph4nt0mgh0sT17@gmail.com>
2022-05-02 12:46:27 +0200

    New changes

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
 import { TitleHistoryComponent } from './title-history/title-history.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TitleHistoryComponent
+    TitleHistoryComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
--- angular-client/src/main.ts
+++ angular-client/src/main.ts
@@ -9,4 +9,5 @@ if (environment.production) {
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
+  .then(success => console.log(`'$'`))
   .catch(err => console.error(err));
