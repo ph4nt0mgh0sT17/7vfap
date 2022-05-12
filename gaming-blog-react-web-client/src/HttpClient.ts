44a8f486ae0fb30f993e6266faf1db4fd40c00b6
David Miko <ph4nt0mgh0sT17@gmail.com>
2022-05-12 20:15:18 +0200

    Added intercepting axios client to retrieve authorization bearer jwt token when user is logged in. :-]

--- gaming-blog-react-web-client/src/HttpClient.ts
+++ gaming-blog-react-web-client/src/HttpClient.ts
@@ -1,5 +1,19 @@
 import axios from "axios";
+import {LoginResponse} from "./Models/Responses/LoginResponse";
 
-const HttpClient = axios.create();
+const HttpClient = axios.create({});
+
+HttpClient.interceptors.request.use(
+    config => {
+        const token = localStorage.getItem('user');
+        // @ts-ignore
+        let loginResponse: LoginResponse = JSON.parse(token);
+        const auth = loginResponse?.jwtToken ? `Bearer ${loginResponse?.jwtToken}` : '';
+        // @ts-ignore
+        config.headers.common['Authorization'] = auth;
+        return config;
+    },
+    error => Promise.reject(error),
+);
 
 export default HttpClient;
--- gaming-blog-react-web-client/src/state/reducers/applicationReducer.ts
+++ gaming-blog-react-web-client/src/state/reducers/applicationReducer.ts
@@ -2,13 +2,20 @@ import {Action} from "../actions";
 import {ActionTypes} from "../action-types";
 import {LoginResponse} from "../../Models/Responses/LoginResponse";
 
-const initialState: LoginResponse | null = null;
+let initialState: LoginResponse | null = null;
+
+let userJsonText = localStorage.getItem('user');
+if (userJsonText != null) {
+    initialState = JSON.parse(userJsonText) as LoginResponse | null;
+}
 
 const reducer = (state: LoginResponse | null = initialState, action: Action): LoginResponse | null => {
     switch (action.type) {
         case ActionTypes.LOGIN:
+            localStorage.setItem('user', JSON.stringify(action.payload));
             return action.payload;
         case ActionTypes.LOGOUT:
+            localStorage.removeItem('user');
             return null;
         default:
             return state;
